8d4d26fe03fb233176a0e122b0e676cac34e3c41
Dharmik7 <dholariyadharmik7@gmail.com>
2022-12-31 14:31:48 -0600

    added the button below the text area

--- pages/index.js
+++ pages/index.js
@@ -18,21 +18,26 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div>
-        <textarea
-          style={{
-            flex: 1,
-            height: "50vh",
-            width: "100vh",
-            backgroundColor: "black",
-            color: "white",
-            padding: "15px",
-            fontWeight:"600",
-            fontSize:"20px",
-            borderRadius:10
-          }}
-          placeholder="Write Something"
-        />
+      <div style={{ flexDirection: "row" }}>
+        <div>
+          <textarea
+            style={{
+              flex: 1,
+              height: "50vh",
+              width: "100vh",
+              backgroundColor: "black",
+              color: "white",
+              padding: "15px",
+              fontWeight: "600",
+              fontSize: "20px",
+              borderRadius: 10,
+            }}
+            placeholder="Write Something"
+          />
+        </div>
+        <div style={{  display: "flex", alignItems:"center",justifyContent:"center",padding:"10px"}}>
+          <button style={{  padding:"10px"}}>Generate</button>
+        </div>
       </div>
     </div>
   );
